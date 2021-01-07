be62c70780746494fa17f4ade2a02a320179b40e
izszzz <izszzz@.yahoo.co.jp>
2021-01-08 01:31:58 +0900

    add prev next btn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ApplicationRecord
     acts_as_taggable_on :tags, :categories
     validates_presence_of :title, :summary, :body
+    def next
+        self.class.where("id > ?", id).first
+    end
+
+    def prev
+        self.class.where("id < ?", id).last
+    end
 end
