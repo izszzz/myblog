a54c13b01e1f18212096086a99afdd499234b28c
izszzz <izszzz@.yahoo.co.jp>
2021-01-09 21:19:31 +0900

    fix production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                  587,
+      domain:               'gmail.com',
+      user_name:            Rails.application.credentials.gmail[:user_name],
+      password:             Rails.application.credentials.gmail[:password],
+      authentication:       'plain',
+      enable_starttls_auto:  true
+  }
+
   GA.tracker = Rails.application.credentials.google_analytics[:tracker]
 end
