dd39cbc5b3d1aa3e498a068841ee64cfc72a3119
izszzz <izszzz@.yahoo.co.jp>
2021-01-19 19:44:21 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get "posts/tags"
   get "posts/categories"
   get "posts/autocomplete"
-  get '/sitemap', to: redirect("https://s3-ap-northeast-1.amazonaws.com/#{Rails.application.credentials.s3[:name]}/sitemaps/sitemap.xml.gz")
+  get '/sitemap', to: redirect("https://s3-ap-northeast-1.amazonaws.com/#{Rails.application.credentials.s3[:name]}/sitemap.xml.gz")
   resources :posts
   resources :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
