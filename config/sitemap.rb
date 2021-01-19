1e50cb4d6cc6f6b0a220207bca502ddd2f47c699
izszzz <izszzz@.yahoo.co.jp>
2021-01-19 19:20:21 +0900

    update aws access key

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,9 +1,9 @@
 # Set the host name for URL creation
 
 SitemapGenerator::Sitemap.default_host = "https://izszzz-blog.herokuapp.com/"
-SitemapGenerator::Sitemap.sitemaps_host= "https://s3-ap-northeast-1.amazonaws.com/#{Rails.application.credentials.s3[:name]}"
+SitemapGenerator::Sitemap.sitemaps_host= "https://s3-ap-northeast-1.amazonaws.com/#{Rails.application.credentials.s3[:bucket_name]}"
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(
-  Rails.application.credentials.s3[:name],
+  Rails.application.credentials.s3[:bucket_name],
   aws_access_key_id: Rails.application.credentials.aws[:access_key],
   aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
   aws_region: 'ap-northeast-1',
